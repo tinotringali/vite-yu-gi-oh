26cd2e821ca3be7cf24a904a1abb47d947633c69
Tino Tringali <115955951+tinotringali@users.noreply.github.com>
2023-03-27 17:17:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Vue 3 + Vite
+
+Tamplate di partenza per progetti con Vue in option API
+
+Per installare il progetto:
+
+-npm install
+-npm run dev
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default{
+  name:'app',
+  components:{
+    AppHeader,
+    AppMain,
+    AppFooter
+
+  }
+}
+</script>
+
+<template>
+
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+
+</template>
+
+<style lang="scss">
+
+
+</style>
