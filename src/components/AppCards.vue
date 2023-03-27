480ee1ff5f901d79861434d54f59fff332fa29a3
tinotringali <tringali.tino94@gmail.com>
2023-03-27 23:25:11 +0200

    aggiunto array tramite Axios

--- src/components/AppCards.vue
+++ src/components/AppCards.vue
@@ -1,18 +1,36 @@
 <script>
+import axios from 'axios';
+
     export default {
-        name:'AppCards'
+        name:'AppCards',
+        data(){
+            return{
+                cardsYu:[]
+            }
+        },
+        created(){
+            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php')
+            .then((response) => {
+                console.log(response)
+                this.cardsYu = response.data.data;
+            })
+        }
         
     }
 </script>
 
 <template>
-    <main>
-        
-    </main>
+    <div class="container">
+        <div class="row">
+            <div class="col-2" v-for="cards in cardsYu">
+                carta
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
-main{
+div{
     background-color: white;
 }
 </style>
